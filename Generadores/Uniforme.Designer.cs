915cfb57c631f337ac82c2210f706d260339f3e5
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-04-15 13:20:13 -0300

    Inicio Uniforme

--- Generadores/Uniforme.Designer.cs
+++ Generadores/Uniforme.Designer.cs
@@ -29,31 +29,32 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtIntervalos = new System.Windows.Forms.TextBox();
             this.lblCantidad = new System.Windows.Forms.Label();
-            this.txtCantidad = new System.Windows.Forms.TextBox();
             this.lblA = new System.Windows.Forms.Label();
             this.lblB = new System.Windows.Forms.Label();
-            this.txtB = new System.Windows.Forms.TextBox();
-            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnGraficar = new System.Windows.Forms.Button();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.boxIntervalos = new System.Windows.Forms.ComboBox();
+            this.dgvUniforme = new System.Windows.Forms.DataGridView();
+            this.nro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtA = new System.Windows.Forms.NumericUpDown();
+            this.txtB = new System.Windows.Forms.NumericUpDown();
+            this.txtCantidad = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUniforme)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidad)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 121);
+            this.label1.Location = new System.Drawing.Point(22, 124);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(144, 16);
             this.label1.TabIndex = 21;
             this.label1.Text = "Cantidad de Intervalos:";
-            // 
-            // txtIntervalos
-            // 
-            this.txtIntervalos.Location = new System.Drawing.Point(182, 117);
-            this.txtIntervalos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtIntervalos.Name = "txtIntervalos";
-            this.txtIntervalos.Size = new System.Drawing.Size(89, 22);
-            this.txtIntervalos.TabIndex = 20;
             // 
             // lblCantidad
             // 
@@ -64,64 +65,178 @@
             this.lblCantidad.TabIndex = 19;
             this.lblCantidad.Text = "Cantidad de Valores:";
             // 
-            // txtCantidad
-            // 
-            this.txtCantidad.Location = new System.Drawing.Point(182, 88);
-            this.txtCantidad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(89, 22);
-            this.txtCantidad.TabIndex = 18;
-            // 
             // lblA
             // 
             this.lblA.AutoSize = true;
-            this.lblA.Location = new System.Drawing.Point(73, 32);
+            this.lblA.Location = new System.Drawing.Point(22, 29);
             this.lblA.Name = "lblA";
-            this.lblA.Size = new System.Drawing.Size(19, 16);
+            this.lblA.Size = new System.Drawing.Size(107, 16);
             this.lblA.TabIndex = 17;
-            this.lblA.Text = "A:";
+            this.lblA.Text = "A (límite inferior) :";
             // 
             // lblB
             // 
             this.lblB.AutoSize = true;
-            this.lblB.Location = new System.Drawing.Point(73, 62);
+            this.lblB.Location = new System.Drawing.Point(22, 61);
             this.lblB.Name = "lblB";
-            this.lblB.Size = new System.Drawing.Size(19, 16);
+            this.lblB.Size = new System.Drawing.Size(113, 16);
             this.lblB.TabIndex = 16;
-            this.lblB.Text = "B:";
+            this.lblB.Text = "B (límite superior):";
             // 
-            // txtB
+            // btnGraficar
             // 
-            this.txtB.Location = new System.Drawing.Point(182, 58);
-            this.txtB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(89, 22);
-            this.txtB.TabIndex = 15;
+            this.btnGraficar.Location = new System.Drawing.Point(182, 168);
+            this.btnGraficar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGraficar.Name = "btnGraficar";
+            this.btnGraficar.Size = new System.Drawing.Size(115, 31);
+            this.btnGraficar.TabIndex = 5;
+            this.btnGraficar.Text = "Graficar";
+            this.btnGraficar.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(25, 168);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(115, 31);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // boxIntervalos
+            // 
+            this.boxIntervalos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.boxIntervalos.FormattingEnabled = true;
+            this.boxIntervalos.Items.AddRange(new object[] {
+            "10",
+            "15",
+            "20",
+            "25"});
+            this.boxIntervalos.Location = new System.Drawing.Point(182, 121);
+            this.boxIntervalos.Name = "boxIntervalos";
+            this.boxIntervalos.Size = new System.Drawing.Size(120, 24);
+            this.boxIntervalos.TabIndex = 4;
+            // 
+            // dgvUniforme
+            // 
+            this.dgvUniforme.AllowUserToAddRows = false;
+            this.dgvUniforme.AllowUserToDeleteRows = false;
+            this.dgvUniforme.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUniforme.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nro,
+            this.valor});
+            this.dgvUniforme.Location = new System.Drawing.Point(317, 29);
+            this.dgvUniforme.Name = "dgvUniforme";
+            this.dgvUniforme.ReadOnly = true;
+            this.dgvUniforme.RowHeadersVisible = false;
+            this.dgvUniforme.RowHeadersWidth = 51;
+            this.dgvUniforme.RowTemplate.Height = 24;
+            this.dgvUniforme.Size = new System.Drawing.Size(439, 217);
+            this.dgvUniforme.TabIndex = 25;
+            // 
+            // nro
+            // 
+            this.nro.HeaderText = "Número";
+            this.nro.MinimumWidth = 6;
+            this.nro.Name = "nro";
+            this.nro.ReadOnly = true;
+            this.nro.Width = 125;
+            // 
+            // valor
+            // 
+            this.valor.HeaderText = "Valor";
+            this.valor.MinimumWidth = 6;
+            this.valor.Name = "valor";
+            this.valor.ReadOnly = true;
+            this.valor.Width = 125;
             // 
             // txtA
             // 
-            this.txtA.Location = new System.Drawing.Point(182, 29);
-            this.txtA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtA.DecimalPlaces = 2;
+            this.txtA.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.txtA.Location = new System.Drawing.Point(182, 27);
+            this.txtA.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.txtA.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
             this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(89, 22);
-            this.txtA.TabIndex = 14;
+            this.txtA.Size = new System.Drawing.Size(120, 22);
+            this.txtA.TabIndex = 1;
+            this.txtA.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtB
+            // 
+            this.txtB.DecimalPlaces = 2;
+            this.txtB.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.txtB.Location = new System.Drawing.Point(182, 59);
+            this.txtB.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.txtB.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(120, 22);
+            this.txtB.TabIndex = 2;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(182, 87);
+            this.txtCantidad.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(120, 22);
+            this.txtCantidad.TabIndex = 3;
             // 
             // Uniforme
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtIntervalos);
-            this.Controls.Add(this.lblCantidad);
             this.Controls.Add(this.txtCantidad);
-            this.Controls.Add(this.lblA);
-            this.Controls.Add(this.lblB);
             this.Controls.Add(this.txtB);
             this.Controls.Add(this.txtA);
+            this.Controls.Add(this.dgvUniforme);
+            this.Controls.Add(this.boxIntervalos);
+            this.Controls.Add(this.btnGraficar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblB);
             this.Name = "Uniforme";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Uniforme";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUniforme)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidad)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,12 +245,17 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtIntervalos;
         private System.Windows.Forms.Label lblCantidad;
-        private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.Label lblA;
         private System.Windows.Forms.Label lblB;
-        private System.Windows.Forms.TextBox txtB;
-        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnGraficar;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ComboBox boxIntervalos;
+        private System.Windows.Forms.DataGridView dgvUniforme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valor;
+        private System.Windows.Forms.NumericUpDown txtA;
+        private System.Windows.Forms.NumericUpDown txtB;
+        private System.Windows.Forms.NumericUpDown txtCantidad;
     }
 }
--- Generadores/Uniforme.cs
+++ Generadores/Uniforme.cs
@@ -15,6 +15,42 @@ namespace SIM_TP2.Generadores
         public Uniforme()
         {
             InitializeComponent();
+            boxIntervalos.SelectedIndex = 0;
+            txtA.Value= 0;
+            txtB.Value = 1;
+            txtCantidad.Value = 10;
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (txtCantidad.Text is null)
+            {
+                txtCantidad.Text = "10";
+            }
+
+            double min = Convert.ToDouble(txtA.Text); // límite inferior
+            double max = Convert.ToDouble(txtB.Text); // límite superior
+            int n = int.Parse(txtCantidad.Text);  // tamaño de muestra
+
+            if (min>max)
+            {
+                double temp = min; min = max; max = temp;
+                txtA.Text = min.ToString();
+                txtB.Text = max.ToString();
+            }
+
+            List<double> lista = new List<double>();
+            dgvUniforme.Rows.Clear();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                double valor = Math.Round(rnd.NextDouble() * (max - min) + min, 4);
+
+                lista.Add(valor);
+                dgvUniforme.Rows.Add((i+1).ToString(), valor.ToString());
+            }
         }
     }
 }
