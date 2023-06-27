f9579be87c7eb222657805ac3a2b2a19b5f5a8d5
Bianca Cano <biancacano2002@gmail.com>
2023-06-27 17:53:00 -0300

    dgv integracion

--- TP5/GrillaIntegracionNumerica.Designer.cs
+++ TP5/GrillaIntegracionNumerica.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GrillaIntegracionNumerica));
             this.dgv_integracion_numerica = new System.Windows.Forms.DataGridView();
             this.t = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,65 +44,93 @@
             // 
             this.dgv_integracion_numerica.AllowUserToAddRows = false;
             this.dgv_integracion_numerica.AllowUserToDeleteRows = false;
+            this.dgv_integracion_numerica.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_integracion_numerica.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.dgv_integracion_numerica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_integracion_numerica.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Gold;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Purple;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_integracion_numerica.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_integracion_numerica.ColumnHeadersHeight = 50;
             this.dgv_integracion_numerica.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.t,
             this.di,
             this.dDidt,
             this.hdDidt,
             this.Diproximo});
-            this.dgv_integracion_numerica.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_integracion_numerica.GridColor = System.Drawing.Color.Indigo;
             this.dgv_integracion_numerica.Location = new System.Drawing.Point(0, 0);
+            this.dgv_integracion_numerica.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgv_integracion_numerica.Name = "dgv_integracion_numerica";
             this.dgv_integracion_numerica.ReadOnly = true;
             this.dgv_integracion_numerica.RowHeadersVisible = false;
+            this.dgv_integracion_numerica.RowHeadersWidth = 51;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("SimSun", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Indigo;
+            this.dgv_integracion_numerica.RowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dgv_integracion_numerica.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv_integracion_numerica.Size = new System.Drawing.Size(530, 450);
+            this.dgv_integracion_numerica.Size = new System.Drawing.Size(892, 554);
             this.dgv_integracion_numerica.TabIndex = 0;
+            this.dgv_integracion_numerica.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_integracion_numerica_CellContentClick);
             // 
             // t
             // 
             this.t.HeaderText = "t";
+            this.t.MinimumWidth = 6;
             this.t.Name = "t";
             this.t.ReadOnly = true;
-            this.t.Width = 35;
+            this.t.Width = 48;
             // 
             // di
             // 
             this.di.HeaderText = "Di";
+            this.di.MinimumWidth = 6;
             this.di.Name = "di";
             this.di.ReadOnly = true;
-            this.di.Width = 42;
+            this.di.Width = 58;
             // 
             // dDidt
             // 
             this.dDidt.HeaderText = "Di\'";
+            this.dDidt.MinimumWidth = 6;
             this.dDidt.Name = "dDidt";
             this.dDidt.ReadOnly = true;
-            this.dDidt.Width = 44;
+            this.dDidt.Width = 68;
             // 
             // hdDidt
             // 
             this.hdDidt.HeaderText = "h*Di\'";
+            this.hdDidt.MinimumWidth = 6;
             this.hdDidt.Name = "hdDidt";
             this.hdDidt.ReadOnly = true;
-            this.hdDidt.Width = 54;
+            this.hdDidt.Width = 88;
             // 
             // Diproximo
             // 
             this.Diproximo.HeaderText = "Di+1";
+            this.Diproximo.MinimumWidth = 6;
             this.Diproximo.Name = "Diproximo";
             this.Diproximo.ReadOnly = true;
-            this.Diproximo.Width = 54;
+            this.Diproximo.Width = 78;
             // 
             // GrillaIntegracionNumerica
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.ClientSize = new System.Drawing.Size(882, 553);
             this.Controls.Add(this.dgv_integracion_numerica);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GrillaIntegracionNumerica";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Grilla Integracion Numerica";
